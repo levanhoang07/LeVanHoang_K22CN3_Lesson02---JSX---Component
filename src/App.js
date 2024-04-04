f55c4a21a0ba19a89fe0c616c77c2a133825518d
levanhoang07 <levanhoang742004@gmail.com>
2024-04-04 11:16:14 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Func_JSX_LeVanHoang from './components/Func_JSX_LeVanHoang'
+import Class_LeVanHoang from './components/Class_LeVanHoang';
+function App(){
+  return(
+    <section ClassName="App" style={{ textAlign: "center" }}>
+      <h1>Demo JSX</h1>
+      {/*function componenet demo*/}
+      <Func_JSX_LeVanHoang/>
+      <Func_JSX_LeVanHoang fullname="Le Van Hoang" age="20"/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Class_LeVanHoang/>
+      <hr/>
+      <Class_LeVanHoang info="Học ReactJS" time="11"/>
+
+    </section>
+  )
 }
-
 export default App;
